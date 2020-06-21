3c27ced477967216474b9fd7085f1fa15bb18da6
netkas <netkas@intellivoid.info>
2020-06-21 14:57:04 -0400

    Updated clean data tool

--- Language Detection/clean_data.sh
+++ Language Detection/clean_data.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 for file in langdetect/*; do
     [ -f "$file" ] || continue
+    echo "Processing ${file}"
     sort -u "${file}" > "${file}.clean"
     rm "${file}"
     mv "${file}.clean" "${file}"
